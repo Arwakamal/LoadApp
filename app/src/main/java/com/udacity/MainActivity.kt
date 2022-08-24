0e1e1664a7cf1fdc5269043bd4e40d33d9561a0d
arwakamal <arwakamal336@gmail.com>
2022-08-24 03:29:43 +0200

    commit

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -12,12 +12,9 @@ import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -29,9 +26,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
+    private var URL = ""
+    private var projectName=""
 
-    private var selectedURL: String? = null
-    private var selectedFileName: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,121 +39,109 @@ class MainActivity : AppCompatActivity() {
 
         custom_button.setOnClickListener {
 
-            if(selectedURL != null) {
-                custom_button.buttonState = ButtonState.Loading
-                download()
-
+            if (radio_btn_glide.isChecked == false && radio_btn_load_app.isChecked == false && radio_btn_retrofit.isChecked == false) {
+                Toast.makeText(this, "Please select file", Toast.LENGTH_SHORT).show()
             } else {
-                val toast = Toast.makeText(
-                    applicationContext,
-                    getString(R.string.please_select_file),
-                    Toast.LENGTH_LONG)
-                toast.show()
+
+                setDownloadURL()
+                download()
+                custom_button.buttonState = ButtonState.Loading
             }
         }
 
         createChannel(CHANNEL_ID, getString(R.string.notification_channel_name))
     }
 
+    private fun setDownloadURL() {
+        if (radio_btn_glide.isChecked) {
+            this.URL = "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+            projectName="Glide Project"
+        }
+        if (radio_btn_load_app.isChecked){
+            projectName="Load app project"
+            this.URL = "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+        }
+        if(radio_btn_retrofit.isChecked) {
+            projectName="Retrofit Project"
+            this.URL = "https://github.com/square/retrofit/archive/refs/heads/master.zip"
+        }
+    }
 
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
 
-private val receiver = object : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            custom_button.buttonState = ButtonState.Completed
 
-        custom_button.buttonState = ButtonState.Completed
+            val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
 
-        val downloadManager = getSystemService(AppCompatActivity.DOWNLOAD_SERVICE) as DownloadManager
+            val query = DownloadManager.Query()
+            query.setFilterById(id!!)
 
-        val query = DownloadManager.Query()
-        query.setFilterById(id!!)
+            val cursor = downloadManager.query(query)
+            if (cursor.moveToFirst()) {
+                val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
 
-        val cursor = downloadManager.query(query)
-        if (cursor.moveToFirst()) {
-            val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                var downloadStatus = "Fail"
+                if (DownloadManager.STATUS_SUCCESSFUL == status) {
+                    downloadStatus = "Success"
+                }
 
-            var downloadStatus = "Fail"
-            if (DownloadManager.STATUS_SUCCESSFUL == status) {
-                downloadStatus = "Success"
+                val toast = Toast.makeText(
+                        applicationContext,
+                        getString(R.string.notification_description),
+                        Toast.LENGTH_LONG)
+                toast.show()
+
+                val notificationManager = getSystemService(NotificationManager::class.java)
+                notificationManager.sendNotification(
+                        CHANNEL_ID,
+                        getString(R.string.notification_description),
+                        applicationContext,
+                        downloadStatus,
+                        projectName
+                )
             }
+        }
+    }
 
-            val toast = Toast.makeText(
-                applicationContext,
-                getString(R.string.notification_description),
-                Toast.LENGTH_LONG)
-            toast.show()
 
-            val notificationManager = getSystemService(NotificationManager::class.java)
-            notificationManager.sendNotification(
-                CHANNEL_ID,
-                getString(R.string.notification_description),
-                applicationContext,
-                downloadStatus,
-                selectedFileName!!
+
+    private fun download() {
+        val request =
+                DownloadManager.Request(Uri.parse(URL))
+                        .setTitle(getString(R.string.app_name))
+                        .setDescription(getString(R.string.app_description))
+                        .setRequiresCharging(false)
+                        .setAllowedOverMetered(true)
+                        .setAllowedOverRoaming(true)
+
+        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        downloadID = downloadManager.enqueue(request)
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                    channelId,
+                    channelName,
+                    NotificationManager.IMPORTANCE_HIGH)
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = getString(R.string.notification_channel_name_description)
+
+            val notificationManager = getSystemService(
+                    NotificationManager::class.java
             )
+            notificationManager.createNotificationChannel(notificationChannel)
         }
     }
-}
+    companion object {
 
-
-
-private fun download() {
-
-    val request = DownloadManager.Request(Uri.parse(selectedURL))
-        .setTitle(getString(R.string.app_name))
-        .setDescription(getString(R.string.app_description))
-        .setRequiresCharging(false)
-        .setAllowedOverMetered(true)
-        .setAllowedOverRoaming(true)
-
-    val downloadManager = getSystemService(AppCompatActivity.DOWNLOAD_SERVICE) as DownloadManager
-    downloadID =
-        downloadManager.enqueue(request)
-}
-
-   companion object {
-    private const val GLIDE_URL =
-        "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
-    private const val URL =
-        "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/masterXXX.zip"
-    private const val RETROFIT_URL =
-        "https://github.com/square/retrofit/archive/refs/heads/master.zip"
-
-    private const val CHANNEL_ID = "channelId"
-    }
-     fun onDownloadBtnClicked(view: View) {
-        if (view is RadioButton && view.isChecked) {
-
-            when (view.getId()) {
-                R.id.radio_glide ->
-                    selectedURL = GLIDE_URL
-                R.id.radio_load_app ->
-                    selectedURL = URL
-                R.id.radio_retrofit ->
-                    selectedURL = RETROFIT_URL
-            }
-
-            selectedFileName = view.text.toString()
-        }
+        private const val CHANNEL_ID = "channelId"
     }
 
-private fun createChannel(channelId: String, channelName: String) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        val notificationChannel = NotificationChannel(
-            channelId,
-            channelName,
-            NotificationManager.IMPORTANCE_HIGH)
-
-        notificationChannel.enableLights(true)
-        notificationChannel.lightColor = Color.RED
-        notificationChannel.enableVibration(true)
-        notificationChannel.description = getString(R.string.notification_channel_name_description)
-
-        val notificationManager = getSystemService(
-            NotificationManager::class.java
-        )
-        notificationManager.createNotificationChannel(notificationChannel)
-    }
- }
 
 }
